e825505eafd4e65f12d0d4dc6117b635fd155d93
Evgeny <117734025+JeffreyWillims@users.noreply.github.com>
2024-10-02 18:28:10 +0300

    Add files via upload

---
+++ suite_12_3.py
@@ -0,0 +1,9 @@
+import unittest
+import test_12_3
+
+runner_suite = unittest.TestSuite()
+runner_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(test_12_3.RunnerTest))
+runner_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(test_12_3.TournamentTest))
+
+runner = unittest.TextTestRunner(verbosity=2)
+runner.run(runner_suite)
---
+++ tests_12_4.py
@@ -0,0 +1,86 @@
+import unittest
+from unittest import TestCase
+from rt_with_exceptions import Runner, Tournament
+import logging
+
+logging.basicConfig(level=logging.INFO, filemode="w", filename="runner_tests.log",
+                    encoding="utf-8", format="%(asctime)s -|- %(levelname)s -|- %(message)s")
+
+class RunnerTest(TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_walk(self):
+        try:
+            r1 = Runner('Bully', -5)
+            for i in range(10):
+                r1.walk()
+            self.assertEqual(r1.distance, 50, f"{r1.name} прошел {r1.distance} а должен был 50")
+        except ValueError as exc:
+            logging.warning("Неверная скорость для Runner", exc_info=exc)
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_run(self):
+        try:
+            r2 = Runner(5)
+            for i in range(10):
+                r2.run()
+            self.assertEqual(r2.distance, 100,f"{r2.name} прошел {r2.distance} а должен был 100")
+            logging.info('"test_run" выполнен успешно')
+        except TypeError as exc:
+            logging.warning("Неверный тип данных для объекта Runner", exc_info=exc)
+
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_challenge(self):
+        r1 = Runner("Bully")
+        r2 = Runner("Alex")
+        for _ in range(10):
+            r1.run()
+            r2.walk()
+        self.assertNotEqual(r1.distance, r2.distance)
+
+
+class TournamentTest(unittest.TestCase):
+
+    is_frozen = True
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = []
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def setUp(self):
+        self.r1 = Runner("Усейн", 10)
+        self.r2 = Runner("Андрей", 9)
+        self.r3 = Runner("Ник", 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for i, elem in enumerate(cls.all_results):
+            print(f'{i + 1}, {elem}')
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_tournament_1(self):
+        t1 = Tournament(90, self.r1, self.r3)
+        t1_result = t1.start()
+        TournamentTest.all_results.append(t1_result)
+        self.assertTrue(t1_result[2], 'Ник')
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_tournament_2(self):
+        t2 = Tournament(90, self.r2, self.r3)
+        t2_result = t2.start()
+        TournamentTest.all_results.append(t2_result)
+        self.assertTrue(t2_result[2], 'Ник')
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_tournament_3(self):
+        t3 = Tournament(90, self.r1, self.r3, self.r3)
+        t3_result = t3.start()
+        TournamentTest.all_results.append(t3_result)
+        self.assertTrue(t3_result[3], 'Ник')
+
+
+if __name__ == "__main__":
+    unittest.main()
